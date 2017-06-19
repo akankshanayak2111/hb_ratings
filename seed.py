1e002a01db294ae5683baf75c97694829885a373
Hackbright Student <no-reply@hackbrightacademy.com>
2017-06-19 16:32:17 -0700

    Reading data from seed file

--- seed.py
+++ seed.py
@@ -1,9 +1,11 @@
 """Utility file to seed ratings database from MovieLens data in seed_data/"""
 
 from sqlalchemy import func
+from datetime import datetime
+
 from model import User
-# from model import Rating
-# from model import Movie
+from model import Rating
+from model import Movie
 
 from model import connect_to_db, db
 from server import app
@@ -36,10 +38,51 @@ def load_users():
 
 def load_movies():
     """Load movies from u.item into database."""
+    print "Movies"
+
+    Movie.query.delete()
+
+    for row in open("seed_data/u.item"):
+        row = row.rstrip()
+        movie_id, movie_title, release_date, vid_release_date, imdb_url = row.split("|")[:5]
+        movie_title = movie_title[:-7]
+
+        if release_date:
+            released_at = datetime.strptime(release_date, "%d-%b-%Y")
+        else:
+            released_at = None
+
+        movie = Movie(movie_id=movie_id,
+                    movie_title=movie_title,
+                    released_at=released_at,
+                    vid_release_date=vid_release_date,
+                    imdb_url=imdb_url)
+
+        db.session.add(movie)
+
+    db.session.commit()
 
 
 def load_ratings():
     """Load ratings from u.data into database."""
+    print "Ratings"
+
+    Rating.query.delete()
+
+    for row in open("seed_data/u.data"):
+        row = row.rstrip()
+        user_id, movie_id, score, timestamp = row.split("\t")
+
+        # format timestamp
+
+        rating = Rating(user_id=user_id,
+                        movie_id=movie_id,
+                        score=score,
+                        timestamp=timestamp)
+
+        db.session.add(rating)
+
+    db.session.commit()
 
 
 def set_val_user_id():
